575f50088ab1356e342f53d1089dc0a0d0dea949
innocent kagina <117772198+innocentkagina@users.noreply.github.com>
2025-05-20 16:21:53 +0300

    Update ddl-bronze.sql

--- scripts/ddl-bronze.sql
+++ scripts/ddl-bronze.sql
@@ -8,69 +8,69 @@ Script Purpose:
 ===============================================================================
 */
 
-    DROP TABLE IF EXISTS bronze.crm_cust_info;
+    DROP TABLE IF EXISTS bronze.crm-cust-info;
  
 
 -- SQLINES FOR EVALUATION USE ONLY (14 DAYS)
-CREATE TABLE bronze.crm_cust_info (
-    cst_id              INT,
-    cst_key             VARCHAR(50),
-    cst_firstname       VARCHAR(50),
-    cst_lastname        VARCHAR(50),
-    cst_marital_status  VARCHAR(50),
-    cst_gndr            VARCHAR(50),
-    cst_create_date     DATE
+CREATE TABLE bronze.crm-cust-info (
+    cst-id              INT,
+    cst-key             VARCHAR(50),
+    cst-firstname       VARCHAR(50),
+    cst-lastname        VARCHAR(50),
+    cst-marital-status  VARCHAR(50),
+    cst-gndr            VARCHAR(50),
+    cst-create-date     DATE
 );
 
-    DROP TABLE IF EXISTS bronze.crm_prd_info;
+    DROP TABLE IF EXISTS bronze.crm-prd-info;
  
 
-CREATE TABLE bronze.crm_prd_info (
-    prd_id       INT,
-    prd_key      VARCHAR(50),
-    prd_nm       VARCHAR(50),
-    prd_cost     INT,
-    prd_line     VARCHAR(50),
-    prd_start_dt TIMESTAMP(3),
-    prd_end_dt   TIMESTAMP(3)
+CREATE TABLE bronze.crm-prd-info (
+    prd-id       INT,
+    prd-key      VARCHAR(50),
+    prd-nm       VARCHAR(50),
+    prd-cost     INT,
+    prd-line     VARCHAR(50),
+    prd-start-dt TIMESTAMP(3),
+    prd-end-dt   TIMESTAMP(3)
 );
 
-    DROP TABLE IF EXISTS bronze.crm_sales_details;
+    DROP TABLE IF EXISTS bronze.crm-sales-details;
  
 
-CREATE TABLE bronze.crm_sales_details (
-    sls_ord_num  VARCHAR(50),
-    sls_prd_key  VARCHAR(50),
-    sls_cust_id  INT,
-    sls_order_dt INT,
-    sls_ship_dt  INT,
-    sls_due_dt   INT,
-    sls_sales    INT,
-    sls_quantity INT,
-    sls_price    INT
+CREATE TABLE bronze.crm-sales-details (
+    sls-ord-num  VARCHAR(50),
+    sls-prd-key  VARCHAR(50),
+    sls-cust-id  INT,
+    sls-order-dt INT,
+    sls-ship-dt  INT,
+    sls-due-dt   INT,
+    sls-sales    INT,
+    sls-quantity INT,
+    sls-price    INT
 );
 
-    DROP TABLE IF EXISTS bronze.erp_loc_a101;
+    DROP TABLE IF EXISTS bronze.erp-loc-a101;
  
 
-CREATE TABLE bronze.erp_loc_a101 (
+CREATE TABLE bronze.erp-loc-a101 (
     cid    VARCHAR(50),
     cntry  VARCHAR(50)
 );
 
-    DROP TABLE IF EXISTS bronze.erp_cust_az12;
+    DROP TABLE IF EXISTS bronze.erp-cust-az12;
  
 
-CREATE TABLE bronze.erp_cust_az12 (
+CREATE TABLE bronze.erp-cust-az12 (
     cid    VARCHAR(50),
     bdate  DATE,
     gen    VARCHAR(50)
 );
 
-    DROP TABLE IF EXISTS bronze.erp_px_cat_g1v2;
+    DROP TABLE IF EXISTS bronze.erp-px-cat-g1v2;
  
 
-CREATE TABLE bronze.erp_px_cat_g1v2 (
+CREATE TABLE bronze.erp-px-cat-g1v2 (
     id           VARCHAR(50),
     cat          VARCHAR(50),
     subcat       VARCHAR(50),
