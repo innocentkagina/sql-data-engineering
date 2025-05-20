987a38101aeae01442909422196be7d6452fa850
innocent kagina <117772198+innocentkagina@users.noreply.github.com>
2025-05-20 16:33:28 +0300

    Update init-database.sql

--- scripts/init-database.sql
+++ scripts/init-database.sql
@@ -3,12 +3,12 @@
 Create Database and Schemas
 =============================================================
 Script Purpose:
-    This script creates a new database named 'data-warehouse' after checking if it already exists. 
+    This script creates a new database named 'data_warehouse' after checking if it already exists. 
     If the database exists, it is dropped and recreated. Additionally, the script sets up three schemas 
     within the database: 'bronze', 'silver', and 'gold'.
 
 WARNING:
-    Running this script will drop the entire 'data-warehouse' database if it exists. 
+    Running this script will drop the entire 'data_warehouse' database if it exists. 
     All data in the database will be permanently deleted. Proceed with caution 
     and ensure you have proper backups before running this script.
 */
@@ -16,14 +16,14 @@ WARNING:
 -- Connect to the default database (usually 'postgres' in PostgreSQL)
 \c postgres;
 
--- Drop and recreate the 'data-warehouse' database
-DROP DATABASE IF EXISTS "data-warehouse";
+-- Drop and recreate the 'data_warehouse' database
+DROP DATABASE IF EXISTS "data_warehouse";
 
--- Create the 'data-warehouse' database
-CREATE DATABASE "data-warehouse";
+-- Create the 'data_warehouse' database
+CREATE DATABASE "data_warehouse";
 
--- Connect to the 'data-warehouse' database
-\c "data-warehouse";
+-- Connect to the 'data_warehouse' database
+\c "data_warehouse";
 
 -- Create Schemas
 CREATE SCHEMA bronze;
