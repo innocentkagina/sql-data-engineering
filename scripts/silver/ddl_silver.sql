fc2c5eaef89cf161676d93a366b851d7f09e309b
innocentkagina <innocent.kanushu.kagina@gmail.com>
2025-05-21 10:47:53 +0000

    added create_date column

--- scripts/silver/ddl_silver.sql
+++ scripts/silver/ddl_silver.sql
@@ -19,7 +19,8 @@ CREATE TABLE silver.crm_cust_info (
     cst_lastname        VARCHAR(50),
     cst_marital_status  VARCHAR(50),
     cst_gndr            VARCHAR(50),
-    cst_create_date     DATE
+    cst_create_date     DATE,
+    dwh_create_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
 
     DROP TABLE IF EXISTS silver.crm_prd_info;
@@ -32,7 +33,8 @@ CREATE TABLE silver.crm_prd_info (
     prd_cost     INT,
     prd_line     VARCHAR(50),
     prd_start_dt TIMESTAMP(3),
-    prd_end_dt   TIMESTAMP(3)
+    prd_end_dt   TIMESTAMP(3),
+    dwh_create_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
 
     DROP TABLE IF EXISTS silver.crm_sales_details;
@@ -47,7 +49,8 @@ CREATE TABLE silver.crm_sales_details (
     sls_due_dt   INT,
     sls_sales    INT,
     sls_quantity INT,
-    sls_price    INT
+    sls_price    INT,
+    dwh_create_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
 
     DROP TABLE IF EXISTS silver.erp_loc_a101;
@@ -55,7 +58,8 @@ CREATE TABLE silver.crm_sales_details (
 
 CREATE TABLE silver.erp_loc_a101 (
     cid    VARCHAR(50),
-    cntry  VARCHAR(50)
+    cntry  VARCHAR(50),
+    dwh_create_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
 
     DROP TABLE IF EXISTS silver.erp_cust_az12;
@@ -64,7 +68,8 @@ CREATE TABLE silver.erp_loc_a101 (
 CREATE TABLE silver.erp_cust_az12 (
     cid    VARCHAR(50),
     bdate  DATE,
-    gen    VARCHAR(50)
+    gen    VARCHAR(50),
+    dwh_create_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
 
     DROP TABLE IF EXISTS silver.erp_px_cat_g1v2;
@@ -74,5 +79,6 @@ CREATE TABLE silver.erp_px_cat_g1v2 (
     id           VARCHAR(50),
     cat          VARCHAR(50),
     subcat       VARCHAR(50),
-    maintenance  VARCHAR(50)
+    maintenance  VARCHAR(50),
+    dwh_create_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
